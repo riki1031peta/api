66a4ab4282a1e3124211c9a98ecb27a39afa04f5
riki1031peta <jojoriki31@icloud.com>
2026-09-12 01:29:55 +0900

    cors変更

--- config/cors.php
+++ config/cors.php
@@ -24,6 +24,7 @@ return [
         'http://localhost:3000',
         'http://127.0.0.1:3000',
         'https://dairy-three-green.vercel.app',
+        'https://dopa-log.com',
     ],
 
     'allowed_origins_patterns' => [],
